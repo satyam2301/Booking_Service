70a2e8df9b18b6a50f196915c2de54cf5e4873e9
satyam2301 <satyamj.2301@gmail.com>
2023-08-28 12:23:51 +0530

    added create booking feature

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -3,4 +3,5 @@ dotenv.config();
 
 module.exports = {
   PORT: process.env.PORT,
+  FLIGHT_SERVICE: process.env.FLIGHT_SERVICE,
 };
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const { InfoController } = require('../../controllers');
+const bookingRoutes = require('./booking-routes');
 const router = express.Router();
 
 router.get('/info', InfoController.info);
-
+router.use('/booking', bookingRoutes);
 module.exports = router;
